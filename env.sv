2c417c0c67ad99c02b378e3004c13fb0ea2a5f15
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-24 18:54:11 -0500

    Create env.sv

---
+++ env.sv
@@ -0,0 +1,22 @@
+class env extends uvm_env;
+  
+  `uvm_component_utils(env)
+  
+  function new(string name ="env", uvm_component parent);
+    super.new(name, parent);
+  endfunction  
+  
+  agent i_agent;
+  scoreboard i_scb;
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);   
+    i_agent = agent::type_id::create("i_agent",this);
+    i_scb = scoreboard::type_id::create("i_scb",this);
+  endfunction  
+  
+	function void connect_phase(uvm_phase phase);
+      i_agent.i_monitor.item_collected_port.connect(i_scb.item_collected_export);
+	endfunction
+  
+endclass  
