2599873b921f7e8767aa85c7292b3f7a13ceb862
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-24 18:55:12 -0500

    Create sequencer.sv

---
+++ sequencer.sv
@@ -0,0 +1,13 @@
+class sequencer extends uvm_sequencer #(packet);
+  
+  packet item;
+  
+  `uvm_component_utils(sequencer)
+  
+  function new(string name ="sequencer", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  
+  
+endclass
