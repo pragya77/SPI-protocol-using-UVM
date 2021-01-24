7805313c34327ef9cf4dcbf955b6e010dd2c97f5
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-24 18:53:54 -0500

    Create spi_test.sv

---
+++ spi_test.sv
@@ -0,0 +1,41 @@
+class test extends uvm_test;
+  
+  `uvm_component_utils(test)
+  
+  function new(string name ="test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  env i_env;
+  sequence1 i_seq;
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    i_env = env::type_id::create("i_env", this);
+    i_seq = sequence1::type_id::create("i_seq");    
+  endfunction  
+  
+  task run_phase(uvm_phase phase);
+    
+    phase.raise_objection(this);
+    fork 
+    	begin
+		    i_seq.start(i_env.i_agent.i_sequencer);         
+    	end
+    	begin
+          #20000;
+    	end
+    join_any  
+    phase.drop_objection(this);
+    
+  endtask  
+  
+   function void end_of_elaboration_phase (uvm_phase phase);
+		uvm_top.print_topology;
+   endfunction  
+   
+   function void check_phase(uvm_phase phase);
+    	check_config_usage();
+   endfunction
+  
+endclass  
