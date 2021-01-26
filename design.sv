586f06f2542045c567e549b2bd3ad5e13d36f9fa
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-25 23:05:50 -0500

    Update design.sv

--- design.sv
+++ design.sv
@@ -1,134 +1,145 @@
-`timescale 1ns/100ps 
+ `timescale 1ns/100ps 
 
-//SPI interface
+ //SPI interface
 
-interface spi_interface(input logic mclk,reset);
-  logic start;
-  logic slave_rd_wr;
-  logic master_rd_wr;
-  logic [6:0]slave_address;
-  logic [6:0]master_address;
-  logic [7:0]master_in_data;
-  logic [7:0]master_out_data;
-  logic [7:0]slave_in_data;
-  logic [7:0]slave_out_data;  
-endinterface
+ interface spi_interface(input logic mclk,reset);
+  	logic start;
+  	logic slave_rd_wr;
+  	logic master_rd_wr;
+  	logic [6:0]slave_address;
+  	logic [6:0]master_address;
+  	logic [7:0]master_in_data;
+  	logic [7:0]master_out_data;
+  	logic [7:0]slave_in_data;
+  	logic [7:0]slave_out_data;  
+ endinterface
 
 
 //-----------SPI TOP DUT-------------//
 
-module top_dut(input logic mclk, input logic reset, input logic start, output logic slave_rd_wr, input logic master_rd_wr, output logic [6:0] slave_address, input logic [6:0] master_address, output logic [7:0] master_in_data,input logic [7:0] master_out_data, output logic [7:0] slave_in_data,input logic [7:0] slave_out_data);
+ module top_dut(input logic mclk, input logic reset, input logic start, output logic slave_rd_wr, input logic master_rd_wr, output logic [6:0] slave_address, input logic [6:0] master_address, output logic [7:0] master_in_data,input logic [7:0] master_out_data, output logic [7:0] slave_in_data,input logic [7:0] slave_out_data);
 
-    logic miso,mosi,cs,sclk;
+    	logic miso,mosi,cs,sclk;
                
-    spi_master m(mclk,reset, start, master_rd_wr, master_address, master_in_data, master_out_data, miso, mosi, cs, sclk);
+     	spi_master m(mclk,reset, start, master_rd_wr, master_address, master_in_data, master_out_data, miso, mosi, cs, sclk);
   
-    spi_slave s(sclk, reset, cs, mosi, miso, slave_rd_wr, slave_address, slave_in_data, slave_out_data);
+    	spi_slave s(sclk, reset, cs, mosi, miso, slave_rd_wr, slave_address, slave_in_data, slave_out_data);
     
-endmodule
+ endmodule
 
 //-------------SPI MASTER--------------//
-module spi_master(input logic mclk, input logic reset, input logic start, input logic master_rd_wr, input logic [6:0] master_address, output logic [7:0] master_in_data, input logic [7:0] master_out_data, input logic miso, output logic mosi, output logic cs, output logic sclk);
+ module spi_master(input logic mclk, input logic reset, input logic start, input logic master_rd_wr, input logic [6:0] master_address, output logic [7:0] master_in_data, input logic [7:0] master_out_data, input logic miso, output logic mosi, output logic cs, output logic sclk);
 
-  int count; 
-  logic [7:0] header = 0;
-  logic [7:0] header1;
-  logic [7:0] in_data;
-  logic [7:0] out_data;
+  	int count; 
+  	logic [7:0] header = 0;
+  	logic [7:0] header1;
+  	logic [7:0] in_data;
+  	logic [7:0] out_data;
   
-  assign sclk=mclk;
-  assign header1 = {master_rd_wr, master_address[6:0]};
-  assign master_in_data = in_data;
+  	assign sclk=mclk;
+  	assign header1 = {master_rd_wr, master_address[6:0]};
   
-  always @(posedge mclk) begin
-    	if(!reset)
-     	 	begin
-        		mosi <=0;
-        		cs <=1; 
-			count <= 0;    
-      		end
-    	else begin
-		if(start)begin
-      			if(count == 0)begin
-				header <= header1;
-				if(master_rd_wr == 0) out_data <= master_out_data;
-			end
-
-			if(count == 1) cs <= 0;
-		
-			if(count >= 1 && count <= 8) begin
-				mosi <= header[7]; 
-				header <= header << 1;
-			end
-
-			if( count > 8 && count <=16) begin
-				if(master_rd_wr == 1) begin
-					in_data[0] <= miso;
-					in_data <= in_data << 1;
-				end
-				else begin
-					mosi <= out_data[7];
-					out_data <= out_data << 1;
-				end		
-
-			end
-
-			count <= count + 1;
-            	end
-
-		else begin
-          		count <= 0;
-          		cs <= 1;
-        	end 
+  	always @(posedge cs)begin
+    		master_in_data <=  in_data;
+    		count <= 0;
   	end
-
-   end
   
-endmodule
+  	always @(posedge mclk) begin
+    		if(!reset)
+     	 		begin
+        			mosi <=0;
+        			cs <=1; 
+				count <= 0;    
+      			end
+    		else begin
+			if(start)begin
+      				if(count == 0)begin
+					header <= header1;
+					if(master_rd_wr == 0) out_data <= master_out_data;
+				end
+
+				if(count == 1) cs <= 0;
+		
+				if(count >= 1 && count <= 8) begin
+					mosi <= header[7]; 
+					header <= header << 1;
+				end
+
+          			if( count > 8 && count <=16) begin
+					if(master_rd_wr == 1) begin
+                  				in_data <= {in_data[6:0], miso};
+					end
+					else begin
+						mosi <= out_data[7];
+						out_data <= out_data << 1;
+					end		
+
+				end
+
+				count <= count + 1;
+        	    	end
+
+			else begin        		
+        	  		cs <= 1;
+        		end 
+  		end
+
+   	end
+  
+ endmodule
 
           
-//---------SPI SLAVE------------//
+ //---------SPI SLAVE------------//
 
-module spi_slave(input logic sclk, input logic reset, input logic cs, input logic mosi, output logic miso, output logic slave_rd_wr, output logic [6:0] slave_address, output logic [7:0]slave_in_data, input logic[7:0] slave_out_data);
+ module spi_slave(input logic sclk, input logic reset, input logic cs, input logic mosi, output logic miso, output logic slave_rd_wr, output logic [6:0] slave_address, output logic [7:0]slave_in_data, input logic[7:0] slave_out_data);
 
-  int count_s;
-  logic [7:0]in_data;
-  logic [7:0] in_header = 0;
-  logic [7:0] out_data;
-  assign slave_in_data =  in_data ;
-  assign slave_rd_wr =  in_header[7] ;
-  assign slave_address[6:0] =  in_header[6:0] ;
+  	int count_s;
+  	logic [7:0]in_data;
+  	logic [7:0] in_header = 0;
+  	logic [7:0] out_data;
   
-  always@(negedge sclk) begin
-    	if(!reset) begin
-      		miso <= 0;
+  	always @(posedge sclk) begin
+    		//
+  	end  
+  
+  	always@(negedge sclk) begin
+    		if(!reset) begin
+      			miso <= 0;
 			count_s <= 0;
-    	end
+    		end
 
-    	else begin
+	    	else begin
 
-		if(!cs)begin
-			if(count_s>=0 && count_s<8)begin
-              in_header <= {in_header[6:0], mosi};
-			end
-          if(count_s>=8 && count_s < 16)begin
-					if(slave_rd_wr == 1)begin
+			if(!cs)begin
+				if(count_s>=0 && count_s<8)begin
+              				in_header <= {in_header[6:0], mosi};
+				end
+         
+          			if(count_s>=8 && count_s < 16)begin
+            				if(in_header[7] == 1)begin
 						miso <= out_data[7];
 						out_data <= out_data << 1;
 					end
 					else begin
-                      in_data <= {in_data[6:0], mosi};
+        		              		in_data <= {in_data[6:0], mosi};
 					end
+				end
+          
+				count_s <= count_s + 1;
 			end
-			count_s <= count_s + 1;
-		end
-		else begin
-			count_s <= 0;
-			out_data <= slave_out_data;
-		end
+			else begin			
+				out_data <= slave_out_data;
+			end
 		
-	end
+		end
         
-  end
+  	end
   
-endmodule
+  	always @(posedge cs)begin
+    		slave_in_data <=  in_data;
+    		slave_rd_wr <=  in_header[7] ;
+    		slave_address[6:0] <=  in_header[6:0] ;
+    		count_s <= 0;
+  	end
+  
+ endmodule
