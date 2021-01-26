90ad6f7e9d7f031ca7e694b5dd496fdb2d957708
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-26 13:41:47 -0500

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,3 +1,4 @@
+
  class monitor extends uvm_monitor;
 
 	packet item;
@@ -8,7 +9,7 @@
    
 	uvm_analysis_port #(packet) item_collected_port;
    
-	function new(string name="monitor", uvm_component parent = null);
+   	function new(string name="monitor", uvm_component parent = null);
 		super.new(name,parent);
 		item_collected_port = new("item_collected_port",this);
 	endfunction
@@ -16,19 +17,20 @@
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
                  
-      		if (!uvm_config_db #(virtual spi_interface)::get(this, get_full_name(),"vif", intf))
+	      	if (!uvm_config_db #(virtual spi_interface)::get(this, get_full_name(),"vif", intf))
       			`uvm_error("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"})	
 	endfunction
 
-	task run_phase(uvm_phase phase);   
+	task run_phase(uvm_phase phase);
+         
       		wait(intf.reset == 1'b0);
    	 	wait(intf.reset == 1'b1);
       		`uvm_info(get_type_name(), "IN RUN PHASE OF MONITOR", UVM_LOW)
-
 		forever begin
-          		item = packet::type_id::create("item",this);
+
+			item = packet::type_id::create("item",this);
           		@(posedge intf.start);
-          		repeat(19) @(posedge intf.mclk);
+          		repeat(20) @(posedge intf.mclk);
           		item.slave_rd_wr = intf.slave_rd_wr;
         		item.master_rd_wr = intf.master_rd_wr;
         		item.slave_address = intf.slave_address;
@@ -37,7 +39,8 @@
           		item.master_out_data = intf.master_out_data;
           		item.slave_in_data = intf.slave_in_data;
           		item.slave_out_data = intf.slave_out_data;
-         		item_collected_port.write(item);
+          
+	  		item_collected_port.write(item);
 		end
 	endtask
 
