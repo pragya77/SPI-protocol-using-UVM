74961107d40b2a9266ed2f62cac915c6308adb2b
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-25 23:08:42 -0500

    Update sequencer.sv

--- sequencer.sv
+++ sequencer.sv
@@ -1,17 +1,16 @@
-class sequencer extends uvm_sequencer #(packet);
+ class sequencer extends uvm_sequencer #(packet);
   
-  packet item;
+  	packet item;
   
-  `uvm_component_utils(sequencer)
+  	`uvm_component_utils(sequencer)
   
-  function new(string name ="sequencer", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction
+  	function new(string name ="sequencer", uvm_component parent = null);
+    		super.new(name, parent);
+  	endfunction
   
-  task run_phase(uvm_phase phase);
+  	task run_phase(uvm_phase phase);
     
-    `uvm_info(get_type_name(), "IN RUN PHASE OF SEQUENCER", UVM_LOW)
-    
-  endtask
+    		`uvm_info(get_type_name(), "IN RUN PHASE OF SEQUENCER", UVM_LOW)
+    	endtask
   
-endclass
+ endclass
